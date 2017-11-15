97b6e185da0e2367f7ec5b3e048837e53663b623
Madani KENAB <kenad2@yahoo.fr>
2017-11-15 21:38:13 +0100

    Septième Commit : Ajout d'une clé étrangère à la table achat

--- achat.sql
+++ achat.sql
@@ -1,6 +1,8 @@
 CREATE TABLE ACHAT (
         np integer,
         nc integer,
-        quantite_achete numeric(12,2)
+        quantite_achete numeric(12,2),
+	FOREIGN KEY (np) REFERENCES produit(np),
+	FOREIGN KEY (nc) REFERENCES client(nc)
 );
 
